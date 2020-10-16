996bd341998834a46801ee23f4dc3222c3164be1
kokrui <h1410129@nushigh.edu.sg>
2020-10-16 16:53:36 +0800

    answer model and migrations

--- verifact/forum/admin.py
+++ verifact/forum/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer
 
 
-@admin.register(Question)
+@admin.register(Question, Answer)
 class QuestionAdmin(admin.ModelAdmin):
     pass
--- verifact/forum/models.py
+++ verifact/forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Question(models.Model):
@@ -10,3 +11,22 @@ class Question(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Answer(models.Model):
+    class AnswerTypes(models.IntegerChoices):
+        TRUE = 1, _("True")
+        FALSE = 2, _("False")
+        OTHER = 3, _("Other")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    answer = models.IntegerField(choices=AnswerTypes.choices)
+    text = models.CharField(max_length=2048)
+    citation_url = models.CharField(max_length=2048)
+    citation_title = models.CharField(max_length=2048)
+    credible_count = models.IntegerField()
+    not_credible_count = models.IntegerField()
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "[%s] %s" % (self.get_answer_display(), self.text)
