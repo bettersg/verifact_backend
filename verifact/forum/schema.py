58fce02fb3d586c87bfcf82b8d14582b8332fa36
kokrui <h1410129@nushigh.edu.sg>
2020-10-16 17:29:26 +0800

    add answer and answers queries with basic question_id filtering

--- verifact/forum/schema.py
+++ verifact/forum/schema.py
@@ -1,12 +1,20 @@
 from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
-from .models import Question
+from .models import Question, Answer
 
 
 class QuestionNode(DjangoObjectType):
     class Meta:
         model = Question
+        interfaces = (relay.Node,)
+
+
+class AnswerNode(DjangoObjectType):
+    class Meta:
+        model = Answer
+        filter_fields = {"question_id": ["exact"]}
         interfaces = (relay.Node,)
 
 
@@ -19,5 +27,11 @@ class Query(ObjectType):
     question = relay.Node.Field(QuestionNode)
     questions = relay.ConnectionField(QuestionConnection)
 
+    answer = relay.Node.Field(AnswerNode)
+    answers = DjangoFilterConnectionField(AnswerNode)
+
     def resolve_questions(root, info):
         return Question.objects.all()
+
+    def resolve_answers(root, info):
+        return Answer.objects.all()
