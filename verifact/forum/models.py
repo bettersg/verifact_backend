d6d9b93b1ce786efada76247e401fb1a166a6deb
kokrui <kokruiwong@gmail.com>
2020-10-17 10:28:13 +0800

    Revert "add createAnswer functionality (note enum + FK handling)"
    This reverts commit 9fd44c8fb4d34e159a3062cfa6cfbf6dda9714ea.

--- verifact/forum/models.py
+++ verifact/forum/models.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -14,22 +13,14 @@ class Question(models.Model):
         return self.text
 
 
-class AnswerTypes(Enum):
-    TRUE = 1
-    FALSE = 2
-    OTHER = 3
-
-
 class Answer(models.Model):
+    class AnswerTypes(models.IntegerChoices):
+        TRUE = 1, _("True")
+        FALSE = 2, _("False")
+        OTHER = 3, _("Other")
 
     created_at = models.DateTimeField(auto_now_add=True)
-    answer = models.IntegerField(
-        choices=[
-            (AnswerTypes.TRUE.value, "True"),
-            (AnswerTypes.FALSE.value, "False"),
-            (AnswerTypes.OTHER.value, "Other"),
-        ],
-    )
+    answer = models.IntegerField(choices=AnswerTypes.choices)
     text = models.CharField(max_length=2048)
     citation_url = models.CharField(max_length=2048)
     citation_title = models.CharField(max_length=2048)
--- verifact/forum/schema.py
+++ verifact/forum/schema.py
@@ -1,9 +1,9 @@
-from graphene import Int, DateTime, String, Field, NonNull, ID, Argument, Enum
-from graphene import relay, ObjectType, Mutation, InputObjectType
+from graphene import DateTime, String, Field
+from graphene import relay, ObjectType, Mutation
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .models import Question, Answer, AnswerTypes
+from .models import Question, Answer
 
 
 class QuestionNode(DjangoObjectType):
@@ -32,7 +32,6 @@ class Query(ObjectType):
     answers = DjangoFilterConnectionField(AnswerNode)
 
     def resolve_questions(root, info):
-        print(Question.objects.all())
         return Question.objects.all()
 
     def resolve_answers(root, info):
@@ -62,61 +61,16 @@ class CreateQuestion(Mutation):
         created_at=None,
     ):
         question = Question.objects.create(
-            created_at=created_at,
             text=text,
             citation_url=citation_url,
-            citation_title=citation_title
-            if citation_title is not None
-            else "Untitled",  # TODO: fetch with OpenGraph if not supplied
-            citation_image_url=citation_image_url
-            if citation_image_url is not None
-            else "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Noun_Project_question_mark_icon_1101884_cc.svg/1024px-Noun_Project_question_mark_icon_1101884_cc.svg.png",  # TODO: fetch with OpenGraph if not supplied
+            citation_title=citation_title if citation_title is not None else "Untitled",  # TODO: fetch with OpenGraph if not supplied
+            citation_image_url=citation_image_url if citation_image_url is not None else "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Noun_Project_question_mark_icon_1101884_cc.svg/1024px-Noun_Project_question_mark_icon_1101884_cc.svg.png",  # TODO: fetch with OpenGraph if not supplied
+            created_at=created_at,
         )
 
         question.save()
         return CreateQuestion(question=question)
 
 
-
-class CreateAnswer(Mutation):
-    class Arguments:
-        created_at = DateTime()
-        answer = Argument(Enum.from_enum(AnswerTypes))
-        text = String()
-        citation_url = String()
-        citation_title = String()
-        credible_count = Int()
-        not_credible_count = Int()
-        question_id = ID()
-
-    answer = Field(AnswerNode)
-
-    def mutate(
-        self,
-        info,
-        answer,
-        text,
-        question_id,
-        citation_url=None,
-        citation_title=None,
-        credible_count=0,
-        not_credible_count=0,
-        created_at=None,
-    ):
-        answer = Answer.objects.create(
-            answer=answer,
-            created_at=created_at,
-            text=text,
-            citation_url=citation_url,
-            citation_title=citation_title,
-            credible_count=credible_count,
-            not_credible_count=not_credible_count,
-            question_id=Question.objects.get(pk=question_id), # should we use pk or graphQL ID?
-        )
-        answer.save()
-        return CreateAnswer(answer=answer)
-
-
 class Mutation(ObjectType):
     create_question = CreateQuestion.Field()
-    create_answer = CreateAnswer.Field()
