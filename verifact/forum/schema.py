e708bb7d94933a663ec625e425dabc691582df65
kokrui <kokruiwong@gmail.com>
2020-10-24 22:13:51 +0800

    decrease arguments + URL validation + custom errorhandling + refactoring

--- verifact/forum/schema.py
+++ verifact/forum/schema.py
@@ -1,6 +1,11 @@
 from graphene import relay, ObjectType, Mutation
 from graphene import DateTime, String, Int, Field
 from graphene_django import DjangoObjectType
+import base64
+from graphql import GraphQLError
+from django.db import IntegrityError
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 
 from .models import Question, Answer
 
@@ -42,12 +47,9 @@ class Query(ObjectType):
         return Answer.objects.all()
 
 
-# MUTATIONS
 
-
-class CreateQuestion(Mutation):
+class QuestionCreate(Mutation):
     class Arguments:
-        created_at = DateTime()
         text = String()
         citation_url = String()
         citation_title = String()
@@ -60,36 +62,32 @@ class CreateQuestion(Mutation):
         info,
         text,
         citation_url,
-        citation_title=None,
-        citation_image_url=None,
-        created_at=None,
+        citation_title="",
+        citation_image_url="",
     ):
+        val = URLValidator()
+        try:
+            val(citation_url)
+        except ValidationError:
+            raise GraphQLError('citationUrl has invalid format!')
         question = Question.objects.create(
-            created_at=created_at,
             text=text,
             citation_url=citation_url,
-            citation_title=citation_title
-            if citation_title is not None
-            else "Untitled",  # TODO: fetch with OpenGraph if not supplied, or in future more likely implement separate mutations to fetch OpenGraph metadata, then update with OpenGraph metadata if user is happy with it
+            citation_title=citation_title,
             citation_image_url=citation_image_url
-            if citation_image_url is not None
-            else "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Noun_Project_question_mark_icon_1101884_cc.svg/1024px-Noun_Project_question_mark_icon_1101884_cc.svg.png",  # TODO: same as 3 lines up, OpenGraph/metadata retrieval
-        )
+            )
 
         question.save()
-        return CreateQuestion(question=question)
+        return QuestionCreate(question=question)
 
 
-class CreateAnswer(Mutation):
+class AnswerCreate(Mutation):
     class Arguments:
-        created_at = DateTime()
         answer = String()
         text = String()
         citation_url = String()
         citation_title = String()
-        credible_count = Int()
-        not_credible_count = Int()
-        question_pk = Int()  # if switching to graphQL ID, switch to String
+        question_id = String()
 
     answer = Field(AnswerNode)
 
@@ -98,29 +96,38 @@ class CreateAnswer(Mutation):
         info,
         answer,
         text,
-        question_pk,
-        citation_url=None,
-        citation_title=None,
-        credible_count=0,
-        not_credible_count=0,
-        created_at=None,
+        question_id,
+        citation_url="",
+        citation_title="",
     ):
-        answer = Answer.objects.create(
-            answer=answer,
-            created_at=created_at,
-            text=text,
-            citation_url=citation_url,
-            citation_title=citation_title,
-            credible_count=credible_count,
-            not_credible_count=not_credible_count,
-            question=Question.objects.get(
-                pk=question_pk
-            ),  # should we use pk or graphQL ID? this uses pk. not sure how to fetch via graphql id though
-        )
-        answer.save()
-        return CreateAnswer(answer=answer)
+        val = URLValidator()
+        try:
+            val(citation_url)
+        except ValidationError:
+            raise GraphQLError('citationUrl has invalid format!')
+
+        try:
+            answer = Answer.objects.create(
+                answer=answer,
+                text=text,
+                citation_url=citation_url,
+                citation_title=citation_title,
+                credible_count=0,
+                not_credible_count=0,
+                question=Question.objects.get(
+                    pk=int(base64.b64decode(question_id.encode()).decode().split(':')[1])
+                ),
+            )
+            answer.save()
+        except IntegrityError as e:
+            if str(e.__cause__).startswith('new row for relation "forum_answer" violates check constraint "forum_answer_answer_valid"'): # this works but looks kinda bandaidy
+                raise GraphQLError('answer must be True, False, or Neither')
+            else:
+                raise
+
+        return AnswerCreate(answer=answer)
 
 
 class Mutation(ObjectType):
-    create_question = CreateQuestion.Field()
-    create_answer = CreateAnswer.Field()
+    question_create = QuestionCreate.Field()
+    answer_create = AnswerCreate.Field()
