8de30be8acc51ebe432a4138231f990e8b5a2551
Jordan Cooperman <jordancooperman@gmail.com>
2020-12-08 17:58:11 +0800

    Use global node interface instead of individual instances

--- verifact/schema.py
+++ verifact/schema.py
@@ -4,11 +4,11 @@ import verifact.forum.schema as ForumSchema
 
 
 class Query(ForumSchema.Query, graphene.ObjectType):
-    # This class will inherit from multiple Queries
-    # as we begin to add more apps to our project
-    pass
+    node = graphene.relay.Node.Field()
+
 
 class Mutation(ForumSchema.Mutation, graphene.ObjectType):
     pass
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
