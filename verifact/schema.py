7fa62a6244ff3ffd58ac7eebb42f1dcdd7d96c00
Jordan Cooperman <jordancooperman@gmail.com>
2020-12-30 21:22:38 +0800

    Add userCreate, userUpdate mutations

--- verifact/schema.py
+++ verifact/schema.py
@@ -1,14 +1,24 @@
 import graphene
 import graphql_jwt
+from graphql_jwt.decorators import login_required
 
+from verifact.graph.user.types import UserType
+from verifact.graph.user.mutations import UserCreate, UserUpdate
 import verifact.forum.schema as ForumSchema
 
 
 class Query(ForumSchema.Query, graphene.ObjectType):
     node = graphene.relay.Node.Field()
+    viewer = graphene.Field(UserType)
+
+    @login_required
+    def resolve_viewer(root, info):
+        return info.context.user
 
 
 class Mutation(ForumSchema.Mutation, graphene.ObjectType):
+    user_create = UserCreate.Field()
+    user_update = UserUpdate.Field()
     token_auth = graphql_jwt.relay.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.relay.Verify.Field()
     refresh_token = graphql_jwt.relay.Refresh.Field()
