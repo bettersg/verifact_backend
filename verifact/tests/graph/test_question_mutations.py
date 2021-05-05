68cfd8a0d9f1be0d1882fd94aec13f5ce0c9e54e
Kok Rui <kokruiwong@gmail.com>
2021-05-05 12:19:30 +0800

    test for no citation when creating question/answer

--- verifact/tests/graph/test_question_mutations.py
+++ verifact/tests/graph/test_question_mutations.py
@@ -1,10 +1,12 @@
 import pytest
 from graphql_jwt.exceptions import PermissionDenied
+import random
 
 from verifact.tests import factories
 from verifact.tests.fake import fake
 from verifact.tests.graph.utils import auth_query, no_auth_query
 from verifact.forum.models import Question
+from verifact import error_strings
 
 
 create_mutation = """
@@ -23,7 +25,7 @@ def test_question_create_with_valid_input_returns_question():
     question = factories.Question.build()
     variables = {"input": {
         "text": question.text,
-        "citationUrls": ["https://ogp.me/"]
+        "citationUrls": ["https://ogp.me/"] * random.randint(1,3)
     }}
     res = auth_query(
         viewer,
@@ -33,13 +35,27 @@ def test_question_create_with_valid_input_returns_question():
     assert res.data["questionCreate"]["question"]["text"] == question.text
     assert Question.objects.first() is not None
 
+@pytest.mark.django_db
+def test_answer_create_with_no_citations_returns_error():
+    viewer = factories.User()
+    question = factories.Question.build()
+    variables = {"input": {
+        "text": question.text,
+        "citationUrls": []
+    }}
+    res = auth_query(
+        viewer,
+        create_mutation % "text",
+        variables=variables,
+    )
+    assert res.errors[0].message == error_strings.ANSWER_QUESTION_MINIMUM_ONE_CITATION
 
 @pytest.mark.django_db
 def test_question_create_when_logged_out_returns_permissions_error():
     question = factories.Question.build()
     variables = {"input": {
         "text": question.text,
-        "citationUrls": ["https://ogp.me/"]
+        "citationUrls": ["https://ogp.me/"] * random.randint(1,3)
     }}
     res = no_auth_query(
         create_mutation % "text",
